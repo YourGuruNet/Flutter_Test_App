282f373ffde2f3c81b8356c6a0f39a10fa145a48
yourgurunet <info@yourgurunet.com>
2020-12-09 14:49:55 +0200

    onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
 import "package:flutter/material.dart";
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 //Base setup
 class MyApp extends StatelessWidget {
+  void answerQustions() {
+    print('Answer chosen!');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorit color?',
+      'What\'s is your favorit foot',
+      'Wher you live?'
+    ];
     return MaterialApp(
-      home: Text("Test"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Android App'),
+        ),
+        body: Column(
+          children: [
+            Text('The qustions!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQustions,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQustions,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQustions,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
